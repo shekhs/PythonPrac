5b842d42553a7909d981d6de8af81577098d65cc
shekhs <31767433+shekhs@users.noreply.github.com>
2022-02-12 22:35:27 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,33 @@
+'''
+----------------------------------------------------------------------
+Sample input:
+thing
+night
+
+Sample output:
+True
+
+----------------------------------------------------------------------
+Sample input:
+upgrad
+found
+
+Sample output:
+False
+'''
+
+#take input here
+s1=[x for x in input()]
+s2=[x for x in input()]
+s1=sorted(s1)
+s2=sorted(s2)
+if s1==s2:
+    print(True)
+else:
+    print(False)
+
+
+
+#code here to check if they are anagrams or no
+
+
