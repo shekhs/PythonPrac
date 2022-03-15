5b043fd45dd33e0b444ef607eb70bd965b9b566e
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2022-03-16 01:19:02 +0530

    Update Break the list.py

--- Break the list.py
+++ Break the list.py
@@ -9,7 +9,7 @@ The number of elements in the list need not to be divisible by k.
 For example, if you want to divide the list [1,2,3,4,5,6,7] into chunk size k=4, then the first chunk will be [1,2,3,4] and the second one will have [5,6,7]. i.e. the last chunk need not have k elements.
 The input will have two lines, the first line would have the list and the second line would have the value of k.(the code for taking input has already been written, you should not change that)
 The final output should have the list chunks in different lines..
-""""""
+""""""""""""
 ###
 Sample Input:
 
