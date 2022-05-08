fe69cf7b68d5a916ee9626e827ca3daa6f3683ee
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2022-05-08 22:41:05 +0530

    Update ArmStrong Number.py

--- ArmStrong Number.py
+++ ArmStrong Number.py
@@ -1,8 +1,8 @@
 '''
 Armstrong number
-
+###
 Description
-
+###
 Any number, say n is called an Armstrong number if it is equal to the sum of its digits, where each is raised to the power of number of digits in n.
 
 For example:
