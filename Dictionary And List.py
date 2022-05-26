6996db252cb9f6c468ecd5e781bb8aad52ce337c
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2022-05-26 22:31:18 +0530

    Update Dictionary And List.py

--- Dictionary And List.py
+++ Dictionary And List.py
@@ -1,5 +1,5 @@
 '''
-###
+####
 '''
 input data
 '''
@@ -9,7 +9,7 @@ Sample input:
 {‘Mobile’: [‘Redmi’, ‘Samsung’, ‘Realme’], 
 ‘Laptop’: [‘Dell’, ‘HP’],
 ‘TV’: [‘Videocon’, ‘Sony’] }
-
+###
 Sample output:
 [‘Mobile_Redmi’, ‘Mobile_Samsung’, ‘Mobile_Realme’, ‘Laptop_Dell’, ‘Laptop_HP’, ‘TV_Videocon’, ‘TV_Sony’]
 
