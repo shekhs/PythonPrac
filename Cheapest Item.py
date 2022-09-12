a101a75eeb2a80ffe3a8870cfe1ab62ed5cdf17b
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2022-09-12 15:17:16 +0530

    Update Cheapest Item.py

--- Cheapest Item.py
+++ Cheapest Item.py
@@ -6,7 +6,7 @@ You will be given a dictionary with keys as items and values as their prices. Yo
 ###
 #
 ------------------------------------------------------------------------
-########
+
 ###
 ###
 Sample input:
