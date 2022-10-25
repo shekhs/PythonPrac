356796a42faf17150f3569e1e48e4740929c1938
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2022-10-25 09:35:20 +0530

    Update Dictionary And List.py

--- Dictionary And List.py
+++ Dictionary And List.py
@@ -8,11 +8,11 @@ Sample input:
 {‘Mobile’: [‘Redmi’, ‘Samsung’, ‘Realme’], 
 ‘Laptop’: [‘Dell’, ‘HP’],
 ‘TV’: [‘Videocon’, ‘Sony’] }
-######
+
 Sample output:
 [‘Mobile_Redmi’, ‘Mobile_Samsung’, ‘Mobile_Realme’, ‘Laptop_Dell’, ‘Laptop_HP’, ‘TV_Videocon’, ‘TV_Sony’]
 
-######
+
 Sample input:
 { 'Pen': ['Gel', 'Ink', 'ball'],
 'Mobile': ['Android', 'apple'] }
