b3b86643267fd75ade1048cf9bd87a516fb5b8b5
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2023-01-18 20:41:31 +0530

    Update Chocolate distribution.py

--- Chocolate distribution.py
+++ Chocolate distribution.py
@@ -8,10 +8,10 @@ while(w//3!=0):
     ch += w//3
     w = w//3 + w%3
 print(ch)
-###
+#####
 #start writing your code here
 #dont forget to print the number of chocolates Sanjay can eat
-
+#####
 #GENERALISED
 '''
 Generalised Chocolate
