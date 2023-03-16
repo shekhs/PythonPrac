993aa877781df21ce92aa82df0a12f3f7871ba5c
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2023-03-16 10:26:37 +0530

    Update ArmStrong Number.py

--- ArmStrong Number.py
+++ ArmStrong Number.py
@@ -4,6 +4,7 @@ Description
 ###
 Any number, say n is called an Armstrong number if it is equal to the sum of its digits, where each is raised to the power of number of digits in n.
 ######
+##
 For example:
 153=13+53+33
 Write Python code to determine whether an entered three digit number is an Armstrong number or not. 
@@ -12,6 +13,7 @@ Print "True" if it is an Armstrong number and print "False" if it is not.
 ###
 Sample Input:
 153
+##
 ###
 Sample Output:
 True
