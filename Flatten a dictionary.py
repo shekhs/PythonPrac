77db2a316919de52e6bc21811e987e1a5c89e9e3
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2023-04-21 13:42:14 +0530

    Update Flatten a dictionary.py

--- Flatten a dictionary.py
+++ Flatten a dictionary.py
@@ -6,7 +6,7 @@ Description
 Consider a nested dictionary as follows:
 ##
 {'Fruit': 1, 'Vegetable': {'Cabbage': 2, 'Cauliflower': 3}, 'Spices': 4}
-###
+#
 Your task is to flatten a nested dictionary and join the nested keys with the "_" character. For the above dictionary, the flattened dictionary would be as follows:
 ###
 {'Fruit': 1, 'Vegetable_Cabbage': 2, 'Vegetable_Cauliflower': 3, 'Spices': 4}
