4368eff192fe087639fea074b8dfacec38209977
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2023-04-26 18:12:03 +0530

    Update weird sum.py

--- weird sum.py
+++ weird sum.py
@@ -9,7 +9,7 @@ For example, if n=3 , then you have to find the value of 3+33+333
 
 if n=10, then you have to find the value of 10 + 1010 + 101010 + 10101010 + 1010101010 + 101010101010 + 10101010101010 + 1010101010101010 +101010101010101010+ 10101010101010101010
 
-
+####
 
 
 
