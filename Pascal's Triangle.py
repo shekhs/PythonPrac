aff354148ef6c1873f43414f3b91735e28f404ec
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2023-07-01 14:28:41 +0530

    Update Pascal's Triangle.py

--- Pascal's Triangle.py
+++ Pascal's Triangle.py
@@ -4,19 +4,19 @@ Description
 A pascal's triangle is a very interesting mathematical concept.
 Each number here is a sum of the two numbers directly above it.
 Following is an 8 level Pascal's triangle:
-######
+
 You can read about Pascal's triangle here.
 Your task is to print an nth level of Pascal's triangle.
 The input will contain an integer n.
 The output will contain 1 line of the list of numbers representing the nth row of Pascal's triangle.
-###
+
 Sample Input:
 6
 Sample Output:
-###
+
 [1, 5, 10, 10, 5, 1]
 
-##
+
 '''
 
 #input has been taken for you
