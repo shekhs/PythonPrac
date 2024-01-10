70f1bd6d1d76d7d1595c2ee86696928ba76908b9
Shushank Shekhar <31767433+shekhs@users.noreply.github.com>
2024-01-10 09:17:01 +0530

    Update Vote for food.py

--- Vote for food.py
+++ Vote for food.py
@@ -4,7 +4,7 @@ Description
 Your team is going for camping and you are taking a vote to decide what food to pack for dinner.
 Everyone gets a vote and the food item that gets at least one more than half of the votes wins. None of the items wins if nothing gets at least one more than half votes. Assume that every person gets only one vote.
 The input will contain a list of food items where each occurrence of an item represents one vote. You should print the winning food item as output. If there is no clear winner, print "NOTA". 
-
+##
 Sample Input:
 ["pasta","pasta","pasta","pasta","pasta","paratha","paratha","paratha"]
 Sample Output:
